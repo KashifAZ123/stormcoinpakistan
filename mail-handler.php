3683c08ef126ca87088b88d43e3620b44bc3d917
KashifAZ123 <61817392+KashifAZ123@users.noreply.github.com>
2020-10-16 22:23:02 +0500

    Add files via upload

---
+++ mail-handler.php
@@ -0,0 +1,25 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = $_POST['email']; // this is your Email address
+    $from = "stormcoinpakistan@gmail.com"
+    $password = $_POST['psw'];
+    $user = $_POST['username'];
+    $first_name = $_POST['name'];
+    $subject = "Welcome To StormCoin Pakistan Mobile Banking";
+    
+    $message = "Hey" . $first_name . "! " . "\n\n". "\n\n" . "Here are your account details:" . "\n\n" . "Username: " . $user . "\n\n" . "Password: " . $password . "\n\n" . 
+    "Log in into your account here: Stormcoin.pk/login ";
+   
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    //mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    // echo "Mail Sent. Thank you " . $first_name . ", we will contac  t you shortly.";
+
+    header("Location: Stormcoin.html"); 
+  
+    exit; 
+ 
+    }
+?>
---
+++ scriptdatabase.js
@@ -0,0 +1,63 @@
+var userarray = [
+    { 'username': 'Kashif Az', 'user': 'kashif@Storm123', 'passkey': 'kashif@123' },
+    { 'username': 'Kashif Az', 'user': 'usaid@Storm123', 'passkey': 'usaid@123' },
+    { 'username': 'Kashif Az', 'user': 'abdulsami@Storm123', 'passkey': 'sami@123' },
+    { 'username': 'Kashif Az', 'user': 'usama@Storm123', 'passkey': 'ceo@123' }
+];
+
+// Finding & Filtering Credentials:
+// var x = userarray.find(x => x.user == 'kashif@Storm123');
+// alert(x.user); //alerts username
+
+
+var name;
+var pass;
+var nameval;
+var passval;
+// currentusername = userarray.find(currentusername => currentusername.username);
+// console.log(currentusername.username)
+
+function UserPass() {
+    name = document.getElementById('inputname').value;
+    pass = document.getElementById('inputpass').value;
+    nameval = userarray.find(nameval => nameval.user == name);
+    passval = userarray.find(passval => passval.passkey == pass);
+    var i;
+    i = name.replace('@Storm123','');
+    var x;
+    x =  i.toUpperCase();
+    // console.log(i);
+
+
+    if (nameval && passval) {
+        document.getElementById("form-login").setAttribute("action", "userinterface.html");
+
+        window.localStorage.setItem("currentuser", x);
+    }
+    else if (nameval === undefined || passval === undefined) {
+        document.getElementById("form-login").setAttribute("action", "accessblocker.html");
+
+    } else {
+        var x;
+        x++;
+        console.log(x);
+    } return true
+}   
+
+
+function userlogout() {
+    alert("User Successfully Logged Out.");
+    localStorage.clear();
+    location.href = "Stormcoin.html";
+
+}
+
+function cancelbtn() {
+    localStorage.clear();
+    location.href = "Stormcoin.html";
+}
+
+function NewUser() {
+    
+
+}
